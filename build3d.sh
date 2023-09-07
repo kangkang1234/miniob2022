10a80d91b181f3ca2f48349763b6ae61418ad147
ksj <you@qq.com>
2023-09-07 10:24:49 +0000

    first commit

---
+++ build3d.sh
@@ -0,0 +1,30 @@
+cd deps
+cd libevent
+git checkout release-2.1.12-stable
+mkdir build
+cd build
+cmake .. -DEVENT__DISABLE_OPENSSL=ON
+make
+sudo make install
+
+cd ../../
+
+cd deps
+cd googletest
+mkdir build
+cd build
+cmake ..
+make
+sudo make install
+
+cd ../../
+
+cd deps
+cd jsoncpp
+mkdir build
+cd build
+cmake -DJSONCPP_WITH_TESTS=OFF -DJSONCPP_WITH_POST_BUILD_UNITTEST=OFF ..
+make
+sudo make install
+
+cd ../../
